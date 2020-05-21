b24133e0a8062321571edd0df53387d3718ea489
Trilok <tmunagala@steadyapp.com>
2020-05-21 12:29:26 +0530

    Initial Commit

---
+++ Autolayout2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolayout2
+//
+//  Created by Trilok on 5/21/20.
+//  Copyright © 2020 datapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
